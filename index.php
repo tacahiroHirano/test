08b34ef28628c31e42d45a7e99831e92fdf0ff74
hirano <hrhm.t.and@gmail.com>
2023-06-26 10:04:26 +0900

    [Add] index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+                $text = 'THIS IS TEST';
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+        <head>
+                <meta charset="utf-8">
+                <title></title>
+        </head>
+        <body>
+                <p><?php echo $text; ?></p>
+        </body>
+</html>
